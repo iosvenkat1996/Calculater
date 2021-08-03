120e4ff201c639f4fbca83d8f693e480b1a6faa1
prathap.kommi@xcubelabs.com <venkyatsmb@gmail.com>
2021-08-03 09:54:10 +0530

    ui designs added

--- calculator....../ViewController.swift
+++ calculator....../ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
-
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double")
+        }
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number * 0.01)
+            }
+            
+        }
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+                
+            } else {
+                
+                if numValue == "." {
+                    let isInt = floor(Double(displayLabel.text!)!) == 
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
+    }
 }
 
